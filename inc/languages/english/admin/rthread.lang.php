9de8f60df1a19b34584f5207998154ca032223f2
Kane Valentine <kane@cute.im>
2018-02-13 23:19:34 +0000

    split frontend and admin language files

--- inc/languages/english/admin/rthread.lang.php
+++ inc/languages/english/admin/rthread.lang.php
@@ -3,7 +3,3 @@
 $l['rthread_description'] = 'Very simple MyBB plugin to select a random thread in a forum from the last X days.';
 
 $l['rthread_admin_pluginlibrary_missing'] = 'Add <a href="https://github.com/frostschutz/MyBB-PluginLibrary">PluginLibrary</a> in order to use the plugin.';
-
-$l['rthread_button_text'] = 'Random Thread';
-
-$l['rthread_not_found'] = 'We couldn\'t find an eligible random thread in this forum!';
---
+++ inc/languages/english/rthread.lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$l['rthread_button_text'] = 'Random Thread';
+
+$l['rthread_not_found'] = 'We couldn\'t find an eligible random thread in this forum! \n';
