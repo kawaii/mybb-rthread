4c480973a1a22804ac72e802beb0a0f91b21a347
Kane Valentine <kane@cute.im>
2018-02-14 15:41:49 +0000

    fix typo in language string for thread not found

--- inc/languages/english/rthread.lang.php
+++ inc/languages/english/rthread.lang.php
@@ -2,4 +2,4 @@
 
 $l['rthread_button_text'] = 'Random Thread';
 
-$l['rthread_not_found'] = 'We couldn\'t find an eligible random thread in this forum! \n';
+$l['rthread_not_found'] = 'We couldn\'t find an eligible random thread in this forum!';
